11e5fe2e1ef61ea4ca79ce6a889d9e444f705441
Khanh1916 <289sgsg@gmail.com>
2025-10-13 16:14:19 +0700

    adding LogOut function

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -206,19 +206,29 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = app.sessionManager.RenewToken(r.Context())
+	err = app.sessionManager.RenewToken(r.Context()) // create new token for user session
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	app.sessionManager.Put(r.Context(), "authenticatedUserID", id)
+	app.sessionManager.Put(r.Context(), "authenticatedUserID", id) // adding authen ID for user session into sessionManager
 
 	http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Logout the user...")
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+
+	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully!")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 // // downloadFile phục vụ file tĩnh để tải về
