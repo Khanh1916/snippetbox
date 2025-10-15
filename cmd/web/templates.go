dbb61cdf45d43a14794a0b447ea20abf5f53a6df
Khanh1916 <289sgsg@gmail.com>
2025-10-15 16:29:44 +0700

    updating Table-driven test in HumanDate func

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -51,6 +51,11 @@ func newTemplateCache() (map[string]*template.Template, error) {
 }
 
 func humanDate(t time.Time) string {
+	// Return the empty string if time has the zero value.
+	if t.IsZero() {
+		return ""
+	}
+	// Convert the time to UTC before formatting it.
 	return t.Format("02 Jan 2006 at 15:04")
 }
 
--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -3,13 +3,39 @@ package main
 import (
 	"testing"
 	"time"
+
+	"github.com/Khanh1916/snippetbox/internal/assert"
 )
 
 func TestHumaDate(t *testing.T) {
-	tm := time.Date(2025, 10, 15, 14, 20, 0, 0, time.UTC)
-	hd := humanDate(tm)
 
-	if hd != "15 Oct 2025 at 14:20" {
-		t.Errorf("got %q; want %q", hd, "15 Oct 2025 at 14:20")
+	tests := []struct {
+		name string
+		tm   time.Time
+		want string
+	}{
+		{
+			name: "UTC",
+			tm:   time.Date(2025, 10, 15, 14, 20, 0, 0, time.UTC),
+			want: "15 Oct 2025 at 14:20",
+		},
+		{
+			name: "Empty",
+			tm:   time.Time{},
+			want: "",
+		},
+		{
+			name: "CET",
+			tm:   time.Date(2025, 10, 15, 13, 20, 0, 0, time.FixedZone("CET", 1*60*60)),
+			want: "15 Oct 2025 at 13:20",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hd := humanDate(tt.tm)
+			// Use the new assert.Equal() helper to compare the expected and actual values.
+			assert.Equal(t, hd, tt.want)
+		})
 	}
 }
